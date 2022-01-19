4301d9c530c02530fd56bcd77ff47cc81b270595
Spencer Gunning <swcgunning@gmail.com>
2022-01-19 15:34:17 -0500

    Finish game logic

--- main.js
+++ main.js
@@ -1,67 +1,39 @@
-/*
-Random number generator
+// Return a random number from 1 to the number of sides passed to it
+const dice = sides => Math.ceil(Math.random() * sides);
 
-When this function is called, it returns a random number from 1 to the number of sides passed to it.
-*/
-function dice(sides) {
-    return Math.ceil(Math.random() * sides);
+// Array of choices in the game
+const choices = ["rock", "paper", "scissors"];
+
+// Game logic
+const rockPaperScissors = human => {
+
+    // Roll d3 for computer's choice
+    const cpu = dice(3);
+
+    // Get string detailing the choses made by each player
+    let choiceStr = `You picked <b>${choices[human-1]}</b><br>`;
+    choiceStr += `The computer picked <b>${choices[cpu-1]}</b><br>`;
+
+    // Check to see who won the game
+    // Return a string representing the result and the player choices
+    if (human === cpu) {
+        // Tie gane
+        return `${choiceStr}<b>Tie game!</b>`;
+    } else if (((human > cpu) && !(human === 3 && cpu === 1)) || (human === 1 && cpu === 3)) {
+        // Human wins
+        return `${choiceStr}<b>You win!</b>`;
+    } else {
+        // Computer wins
+        return `${choiceStr}<b>You lose!</b>`;
+    }
 }
 
-/*
-    Game function
-
-    It takes a single argument: humanChoice
-    1 = rock
-    2 = paper
-    3 = scissors
-
-    When this function is called, it:
-    1) Makes the computer choose its position
-    2) Determines who wins
-    3) Displays the play of the game
-    
-*/
-function rockPaperScissors(humanChoice) {
-
-    var choiceName = ["Rock","Paper","Scissors"];
-
-    //STEP 1:
-    //Determine a random choice for the computer
-    //1 is rock
-    //2 is paper
-    //3 is scissors
-    var computerChoice;
-
-    //This boolean value represents if the human won
-    var humanWins = false;
-
-    //This boolean value represents if there's a tie
-    var tied = false;
-
-    //STEP 2:
-    //Check to see who won and set humanWins & tied
-    //You'll need to build additional if statements
-    // if () {}
-
-    //STEP 3:
-    //Build a string to display the results of the game
-    //Put it in the end variable
-    //It should include:
-    //    a) What you chose
-    //    b) What the computer chose
-    //    c) Whether you won or not
-    //You may need if statements somewhere around this task
-    var end;
-
-    //STEP 4:
-    //Reach out into the DOM with getElementById() and insert the end into the result div.
-
-}
-
-//This code links in the Rock button so that when it's clicked it fires off the game.
-document.getElementById("rock").onclick = () => {
-    rockPaperScissors(1);
-};
-
-//STEP 5:
-//Below, link up the other two buttons with an EventListener so that the user can click on them and activate the game.    
+// EventListener function to begin the game on click
+window.addEventListener('click', e => {
+    for (const [i, v] of choices.entries()) {
+        id = document.getElementById(v);
+        if (id.contains(e.target)) {
+            document.getElementById('result').innerHTML = rockPaperScissors(i+1);
+        }
+    }
+});
